f06f5bc371b5a8c4b511cccadaeada33318c1dbb
priiyam <crifte@gmail.com>
2017-12-16 18:34:03 +0530

    created collection, fuction to send status

--- server.js
+++ server.js
@@ -7,5 +7,16 @@ mongo.connect('mongodb://127.0.0.1/mean-socketchat', (err, db) => {
         throw err;
     }
 
+    // Connect to socket.io
+    client.on('connection', () => {
+         //creating a collection
+        let chat = db.collection('chats');
+
+        // creating function to send status
+        sendStatus = (s) => {
+            socket.emit('status', s);
+        }
+    });
+
     console.log('MongoDB connected');
 });
